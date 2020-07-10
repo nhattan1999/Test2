43bc2af54df6bbe17a78057be7e5c162b0e21763
Nhat Tan <nhattantrantruong@gmail.com>
2020-07-10 21:23:27 +0700

    fix sending email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,8 +61,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      user_name: ENV["gmail_username"],
-      password: ENV["gmail_password"],
+      user_name: "abcan1999@gmail.com",
+      password: "rubyonrails@org.com",
       authentication: :plain,
       enable_starttls_auto: true
   }
