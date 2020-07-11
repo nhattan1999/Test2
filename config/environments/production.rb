cc9023b1b3eff3ccdba8d18ec5c7265eb323d28f
Nhat Tan <nhattantrantruong@gmail.com>
2020-07-11 21:04:26 +0700

    blabla

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   #config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'yourapp.heroku.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
